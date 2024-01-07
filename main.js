e13be87ddc1873b72453d6a14011ba7f6190f8ce
Mohamad Mourad <89941937+Mohamadmourad@users.noreply.github.com>
2024-01-07 14:11:56 +0200

    more random colors

--- main.js
+++ main.js
@@ -46,29 +46,8 @@ document.getElementById("yes").onclick = () => {//delete button
 }
 
 function colorSelector() {// to make each time a randoom block color
-    const randomNumber = Math.floor(Math.random() * 10) + 1;
-    let color = "";
-    switch (randomNumber) {
-        case 1: color = "#EC6262"
-            break;
-        case 2: color = "#E1D276"
-            break;
-        case 3: color = "#99EA86"
-            break;
-        case 4: color = "#83D6B7"
-            break;
-        case 5: color = "#E4A8F9"
-            break;
-        case 6: color = "#CDD3FF"
-            break;
-        case 7: color = "#FFDCF1"
-            break;
-        case 8: color = "#E3FFD7"
-            break;
-        case 9: color = "#E4FFF6"
-            break;
-        case 10: color = "#EC6262"
-            break;
-    }
-    return color;
+    const randomNumber = Math.floor(Math.random() * 359);
+    let color = randomNumber;
+    console.log("hsl("+color+", 120%, 81%)");
+    return "hsl("+color+", 120%, 81%)";
 }
